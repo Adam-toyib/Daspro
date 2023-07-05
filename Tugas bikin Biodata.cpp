e3f2f25901e6110e5eca38c717e09f5db4710c02
Adam-toyib <134986353+Adam-toyib@users.noreply.github.com>
2023-07-05 23:53:15 +0700

    Add files via upload

---
+++ Tugas bikin Biodata.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"==============Selamat Datang=============" <<endl;
+	cout<<"Nama            : Adam Toyib Nurwaid" <<endl;
+	cout<<"Jenis kelamin   : Laki Laki" <<endl;
+	cout<<"Prodi           : Teknik Informatika" <<endl;
+	cout<<"Daerah          : Depok" <<endl;
+	cout<<"Pekerjaan       : Mahasiswa" <<endl;
+	cout<<"Alamat          : jl.h.kotong rt05 rw09 no.205" <<endl;
+	cout<<"email           : adamnurwahid65@student.cs.unida.gontor.ac.id" <<endl;
+	cout<<"No.Tlp          : 087846648388" <<endl;
+	cout<<"Nama Orangtua   : Bambang S" <<endl;
+	cout<<"No.Tlp Orangtua : 082112218463" <<endl;
+	cout<<"==============Terima Kasih================" <<endl;
+	return 0;	
+}
+
